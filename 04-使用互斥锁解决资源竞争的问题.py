161e2155bc051d09117fd718e962b45cbecd3708
laoxia123 <1452611972@qq.com>
2020-01-03 23:26:09 +0800

    second

---
+++ 04-使用互斥锁解决资源竞争的问题.py
@@ -0,0 +1,34 @@
+import time
+import threading
+g_num=0
+def test1(num):
+	global g_num
+	#上锁
+	mutex.acquire()
+	for i in range(num):
+		g_num += 1
+	#解锁
+	mutex.release()
+	print("---in test1 g_num=%d---" % g_num)
+
+def test2(num):
+	global g_num
+	mutex.acquire()
+	for i in range(num):
+		g_num += 1
+	mutex.release()
+	print("---in test2 g_num=%d---"% g_num)
+#创建一个互斥锁，默认是没有上锁的
+mutex = threading.Lock()
+def main():
+	#target指定这个线程去哪个函数执行代码
+	#args传递参数过去
+	t1 = threading.Thread(target=test1,args=(100,))
+	t2 = threading.Thread(target=test2,args=(100,))
+	t1.start()
+	t2.start()
+	time.sleep(5)
+	print("---in main Thread g_num=%d---"% g_num)
+
+if __name__=="__main__":
+	main()
